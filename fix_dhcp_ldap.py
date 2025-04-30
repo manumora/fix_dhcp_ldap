86744e4cb915bc78b1b2300e9810fcd006a7128d
Manu <manuel@fusiontelecom.co>
2025-04-30 11:59:22 +0200

    Added header

--- fix_dhcp_ldap.py
+++ fix_dhcp_ldap.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 # filepath: fix_dhcp_ldap.py
 
+##############################################################################
+# -*- coding: utf-8 -*-
+# Project:      Fix DHCP LDAP
+# Module:       fix_dhcp_ldap.py
+# Purpose:      Modify DHCP entries in LDAP
+# Language:     Python 3
+# Date:         April-2025
+# Author:       Manuel Mora Gordillo
+# Copyright:    2025 - Manuel Mora Gordillo <manuel.mora.gordillo @no-spam@ gmail.com>
+#
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+##############################################################################
+
 import ldap3
 import logging
 import sys
